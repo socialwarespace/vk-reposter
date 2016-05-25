bf27fd08a34169ddcec520e15b36e5e4633cf02e
Alex Uralov <a.uralov@wis.eu.com>
2016-05-25 14:55:14 +0300

    Добавил инсталл скрипт

---
+++ etc/install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR
+cd ../
+PROJECT_ROOT=`pwd`
+
+LOG_DIR="$PROJECT_ROOT/../log"
+ENV_DIR="$PROJECT_ROOT/../env"
+PROJECT_SRC="$PROJECT_ROOT/src"
+PYTHON_VERSION='python3'
+
+sudo apt-get install -y nginx
+sudo apt-get install -y supervisor
+sudo apt-get install -y virtualenv
+sudo apt-get install -y pip
+sudo apt-get install -y git
+
+mkdir -p $LOG_DIR
+mkdir -p $ENV_DIR
+
+# create env and install requirements
+cd $PROJECT_ROOT
+virtualenv -p $PYTHON_VERSION $ENV_DIR
+source $ENV_DIR/bin/activate
+pip install -r requirements.txt
+
+# init project
+cd $PROJECT_SRC
+python manage.py migrate
+python manage.py collectstatic
+python manage.py createsuperuser
